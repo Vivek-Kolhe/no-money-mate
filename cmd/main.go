c72440632e0cc71b37401059b807f1721e3036fd
Vivek Kolhe <vivekkolhe694@gmail.com>
2025-07-28 20:29:07 +0530

    cors origin

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Vivek-Kolhe/no-money-mate/internal/routes"
 	"github.com/Vivek-Kolhe/no-money-mate/internal/services"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -21,6 +22,12 @@ func main() {
 
 	db := models.ConnectDB()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Allow all origins
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
 	userRepo := repository.NewUserRepository(db)
 	userService := services.NewUserService(userRepo)
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.24.5
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.8 // indirect
+	github.com/gofiber/fiber/v2 v2.52.9 // indirect
 	github.com/gofiber/fiber/v3 v3.0.0-beta.4 // indirect
 	github.com/gofiber/schema v1.6.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.13 // indirect
